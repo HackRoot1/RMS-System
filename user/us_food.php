7fd15d0c821f62176028077f763b0dae6719bb23
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-07-22 23:05:25 +0530

    fix: fetched dynamic images

--- user/us_food.php
+++ user/us_food.php
@@ -15,7 +15,7 @@ $foodData = mysqli_fetch_assoc($fetch_food_data);
 
 $css = "us_food";
 include("./header.php");
-
+$set_of_imgs = explode(",", $foodData['set_of_imgs']);
 ?>
 
 
@@ -25,20 +25,17 @@ include("./header.php");
         <section class="food">
             <div class="food-container">
                 <div class="food-items food-1">
-                    <img src="../assets/images/desi-meals.png" alt="" id="food-1">
+                    <img src="../assets/uploads/<?= $foodData['food_name'] ."/" . $set_of_imgs[0] ?>" alt="" id="food-1">
                 </div>
                 <div class="food-items food-2">
-                    <img src="../assets/images/desi-meals2.png" alt="" id="food-2">
+                    <img src="../assets/uploads/<?= $foodData['food_name'] ."/" . $set_of_imgs[1] ?>" alt="" id="food-2">
                 </div>
 
                 <div class="food-items food-3">
-                    <div class="btn">
-                        More..
-                    </div>
-                    <img src="../assets/images/desi-meals3.png" alt="" id="food-3">
+                    <img src="../assets/uploads/<?= $foodData['food_name'] ."/" . $set_of_imgs[2] ?>" alt="" id="food-3">
                 </div>
                 <div class="food-items food-4">
-                    <img src="../assets/images/desi-meals4.png" alt="" id="food-4">
+                    <img src="../assets/uploads/<?= $foodData['food_name'] ."/" . $set_of_imgs[3] ?>" alt="" id="food-4">
                 </div>
             </div>
 
